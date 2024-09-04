0fa0dfa4380c941f06fe55715b585b19908eb6b3
Edison Matos <edison.matos@live.com>
2024-09-04 08:44:32 -0300

    feat: Inset Logo

--- src/components/sidebar/components/Brand.tsx
+++ src/components/sidebar/components/Brand.tsx
@@ -7,11 +7,13 @@ import {
   Image,
   Avatar,
   FormLabel,
+  Img,
 } from '@chakra-ui/react';
 // Custom components
 import { HorizonLogo } from 'components/icons/Icons';
 import { HSeparator } from 'components/separator/Separator';
 import Logo from '../../../../public/img/new/CashLogo.png';
+import pay4gainsLogo from '../../../img/brand/pay4gainsLogo.png';
 export function SidebarBrand() {
   //   Chakra color mode
   let logoColor = useColorModeValue('navy.700', 'white');
@@ -19,15 +21,16 @@ export function SidebarBrand() {
 
   return (
     <Flex alignItems="center" flexDirection="column" className="w-[95%]">
-      {/* <HorizonLogo h='26px' w='175px' my='32px' color={logoColor} /> */}
-      <Flex alignItems="center" flexDirection="row">
+      {/* <HorizonLogo h="26px" w="175px" my="32px" color={logoColor} /> */}
+      <Image alt="" src={pay4gainsLogo.src} w="60%" h="auto" />
+      {/* <Flex alignItems="center" flexDirection="row">
         <Text color={logoColor} fontSize="3xl" ms="5px" fontWeight="700">
           Pay4
         </Text>
         <Text color={secondColor} fontSize="3xl" ms="5px" fontWeight="700">
           Gains
         </Text>
-      </Flex>
+      </Flex> */}
       <HSeparator mb="20px" mt="20px" />
     </Flex>
   );
