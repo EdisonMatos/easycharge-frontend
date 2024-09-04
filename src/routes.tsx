cf05c9a2951372bea2492d84b00fb3598332b282
Edison Matos <edison.matos@live.com>
2024-09-03 21:30:31 -0300

    feat: Rename button on navbar to "Sair"

--- src/routes.tsx
+++ src/routes.tsx
@@ -52,7 +52,7 @@ const routes: IRoute[] = [
   //   icon: <Icon as={MdPerson} width="20px" height="20px" color="inherit" />,
   // },
   {
-    name: 'Sign In',
+    name: 'Sair',
     layout: '/auth',
     path: '/sign-in',
     icon: <Icon as={MdLock} width="20px" height="20px" color="inherit" />,
