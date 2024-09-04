bccafd175d9de9a2ccb769bba0cfd90ead81d5ec
Edison Matos <edison.matos@live.com>
2024-09-03 22:25:26 -0300

    feat: Implement tailwind css

--- src/app/admin/sendReceipt/page.tsx
+++ src/app/admin/sendReceipt/page.tsx
@@ -60,22 +60,51 @@ export default function NftMarketplace() {
   const textColor = useColorModeValue('secondaryGray.900', 'white');
   const textColorBrand = useColorModeValue('brand.500', 'white');
   return (
-    <Box pt={{ base: '180px', md: '80px', xl: '80px' }}>
+    <Box pt={{ base: '180px', md: '80px', xl: '80px' }} className="lg:w-[50%]">
       {/* Main Fields */}
-
       <Flex flexDirection="column">
-        <Card p="0px">
-          <Flex
-            align={{ sm: 'flex-start', lg: 'center' }}
-            justify="space-between"
-            w="100%"
-            px="22px"
-            py="18px"
+        <Card className="px-[25px] py-[20px]" p="0px">
+          <h2 className="mb-[12px]">Como funciona?</h2>
+          <p className="text-[16px] text-neutral-500 font-medium">
+            Após fazer o depósito, utilize o botão abaixo para enviar para nós o
+            comprovante. Após confirmarmos, iremos disponibilizar o seus pontos,
+            que serão convertido em dinheiro e depositados em sua conta. <br />
+            <br />
+            Você pode acompanhar o andamento da sua solicitação em "Meus Envios"
+          </p>
+          <h2 className="mt-[28px] mb-[28px]">
+            Envie seu comprovante de depósito:
+          </h2>
+          <label
+            htmlFor="uploadFile1"
+            className="bg-white text-neutral-500 font-medium text-base rounded max-w-md h-52 flex flex-col items-center justify-center cursor-pointer border-2 border-gray-300 border-dashed mx-auto font-[sans-serif]"
           >
-            <Text color={textColor} fontSize="xl" fontWeight="600">
-              Envie seu comprovante de depósito
-            </Text>
-          </Flex>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="w-11 mb-2 fill-gray-500"
+              viewBox="0 0 32 32"
+            >
+              <path
+                d="M23.75 11.044a7.99 7.99 0 0 0-15.5-.009A8 8 0 0 0 9 27h3a1 1 0 0 0 0-2H9a6 6 0 0 1-.035-12 1.038 1.038 0 0 0 1.1-.854 5.991 5.991 0 0 1 11.862 0A1.08 1.08 0 0 0 23 13a6 6 0 0 1 0 12h-3a1 1 0 0 0 0 2h3a8 8 0 0 0 .75-15.956z"
+                data-original="#000000"
+              />
+              <path
+                d="M20.293 19.707a1 1 0 0 0 1.414-1.414l-5-5a1 1 0 0 0-1.414 0l-5 5a1 1 0 0 0 1.414 1.414L15 16.414V29a1 1 0 0 0 2 0V16.414z"
+                data-original="#000000"
+              />
+            </svg>
+            Clique aqui para anexar a imagem
+            <input type="file" id="uploadFile1" className="hidden" />
+            <p className="text-xs font-medium text-gray-400 mt-2">
+              Formatos permitidos: PNG, JPG SVG, WEBP.
+            </p>
+          </label>
+          <Text
+            color={textColor}
+            className="w-full"
+            fontSize="xl"
+            fontWeight="600"
+          ></Text>
         </Card>
       </Flex>
       {/* Delete Product */}
