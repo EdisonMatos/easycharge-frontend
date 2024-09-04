b254dcaef7d6df6ea7607701361f671e6e332750
Edison Matos <edison.matos@live.com>
2024-09-04 08:16:47 -0300

    feat: Adjust footer position

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -27,7 +27,6 @@ interface DashboardLayoutProps extends PropsWithChildren {
 
 // Custom Chakra theme
 export default function AdminLayout(props: DashboardLayoutProps) {
-
   const { children, ...rest } = props;
   // states and functions
   const [fixed] = useState(false);
@@ -43,57 +42,59 @@ export default function AdminLayout(props: DashboardLayoutProps) {
 
   return (
     <SessionWrappers>
-    <Box h="100vh" w="100vw" bg={bg}>
-      <SidebarContext.Provider
-        value={{
-          toggleSidebar,
-          setToggleSidebar,
-        }}
-      >
-        <Sidebar routes={routes} display="none" {...rest} />
-        <Box
-          float="right"
-          minHeight="100vh"
-          height="100%"
-          overflow="auto"
-          position="relative"
-          maxHeight="100%"
-          w={{ base: '100%', xl: 'calc( 100% - 290px )' }}
-          maxWidth={{ base: '100%', xl: 'calc( 100% - 290px )' }}
-          transition="all 0.33s cubic-bezier(0.685, 0.0473, 0.346, 1)"
-          transitionDuration=".2s, .2s, .35s"
-          transitionProperty="top, bottom, width"
-          transitionTimingFunction="linear, linear, ease"
+      <Box h="100vh" w="100vw" bg={bg}>
+        <SidebarContext.Provider
+          value={{
+            toggleSidebar,
+            setToggleSidebar,
+          }}
         >
-          <Portal>
-            <Box>
-              <Navbar
-                onOpen={onOpen}
-                logoText={'Horizon UI Dashboard PRO'}
-                brandText={getActiveRoute(routes)}
-                secondary={getActiveNavbar(routes)}
-                message={getActiveNavbarText(routes)}
-                fixed={fixed}
-                {...rest}
-              />
-            </Box>
-          </Portal>
-
+          <Sidebar routes={routes} display="none" {...rest} />
           <Box
-            mx="auto"
-            p={{ base: '20px', md: '30px' }}
-            pe="20px"
-            minH="100vh"
-            pt="50px"
+            float="right"
+            minHeight="100vh"
+            height="100%"
+            overflow="auto"
+            position="relative"
+            maxHeight="100%"
+            w={{ base: '100%', xl: 'calc( 100% - 290px )' }}
+            maxWidth={{ base: '100%', xl: 'calc( 100% - 290px )' }}
+            transition="all 0.33s cubic-bezier(0.685, 0.0473, 0.346, 1)"
+            transitionDuration=".2s, .2s, .35s"
+            transitionProperty="top, bottom, width"
+            transitionTimingFunction="linear, linear, ease"
           >
-            {children}
+            {/* Sitemap e navbar = Portal */}
+
+            <Portal>
+              <Box>
+                <Navbar
+                  onOpen={onOpen}
+                  logoText={'Pla 4 Gains'}
+                  brandText={getActiveRoute(routes)}
+                  secondary={getActiveNavbar(routes)}
+                  message={getActiveNavbarText(routes)}
+                  fixed={fixed}
+                  {...rest}
+                />
+              </Box>
+            </Portal>
+
+            <Box
+              mx="auto"
+              p={{ base: '20px', md: '30px' }}
+              pe="20px"
+              minH="95vh"
+              pt="50px"
+            >
+              {children}
+            </Box>
+            <Box>
+              <Footer />
+            </Box>
           </Box>
-          <Box>
-            <Footer />
-          </Box>
-        </Box>
-      </SidebarContext.Provider>
-    </Box>
+        </SidebarContext.Provider>
+      </Box>
     </SessionWrappers>
   );
 }
